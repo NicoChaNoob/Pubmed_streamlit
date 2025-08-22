6d2f241879d858000fabea31f96dd2e71c47152b
NicoChaNoob <nicolas.chan@m4x.org>
2025-08-22 12:26:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ def analyze_extracted_data(articles):
                     {"role": "system", "content": "Tu es un expert en pharmacovigilance qui travaille pour un laboratoire pharmaceutique."},
                     {"role": "user",   "content": prompt}
                 ],
-                temperature=0.7,
+                temperature=1,
                 max_completion_tokens=40000
             )
             summaries.append(resp.choices[0].message.content)
@@ -264,6 +264,7 @@ if st.button("Run Search & Analyze"):
             analysis = analyze_extracted_data(articles)
         st.markdown("### ChatGPT Analysis")
         st.write(analysis)
+
 
 
 
